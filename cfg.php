ba0a50590361af6332b24113c14217754a02a701
Asep Riezky <98480691+AsepRizz@users.noreply.github.com>
2023-09-14 14:35:50 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -4,6 +4,6 @@
 
 $apikey = "eyemrefreeJpc3MiOiJ0b3B0YWwuY29tIiwiGXhwIjoxNDI2NDIwODAwLCtodHwdGFsLmNvbS9d21wYW55IioiVGJ1ZzX1";
 
-$i_id = "52725527677";
+$i_id = "10130082141";
 
-$dID = "b61b951603cf0fad";
+$dID = "c25f051db93d0f2e";
